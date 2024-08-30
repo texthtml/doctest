2bef8a0e1e743d06256f211ee1cc240b4b81ea64
Mathieu Rochette <mathieu@texthtml.net>
2024-08-30 12:13:17 +0200

    Tweak missing exception error message a bit

--- tests/data/examples/exception/missing-exception.php
+++ tests/data/examples/exception/missing-exception.php
@@ -1,3 +1,3 @@
-<?php // Exception Expected to throw "RuntimeException", got "none"
+<?php // Exception Expected to throw "RuntimeException", got none
 
 // @throws RuntimeException This example will fail
