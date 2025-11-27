b3a555e1497535691dd803373540756c3efdc027
Mathieu Rochette <mathieu@rochette.cc>
2025-11-27 11:27:51 +0100

    Prepare for Symfony 8

--- tests/phpstan-application.php
+++ tests/phpstan-application.php
@@ -1,12 +1,7 @@
 <?php declare(strict_types=1);
 
-use Symfony\Component\Console\Application;
-use TH\DocTest\Application as DocTestApplication;
+use TH\DocTest\Application;
 
 require __DIR__ . "/../vendor/autoload.php";
 
-$application = new Application();
-
-$application->add(new DocTestApplication());
-
-return $application;
+return new Application();
